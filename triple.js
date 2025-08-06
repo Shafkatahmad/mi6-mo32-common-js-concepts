65705285695690ab3ec9bcfe1e958fa5cc3ebceb
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-06 23:34:39 +0800

    diff between == and ===

---
+++ triple.js
@@ -0,0 +1,15 @@
+// always use ===
+// equal comparison doesn't work for non-premitive type
+
+const first = [];
+const second = first;
+if(first === second) {
+  console.log('values are equal');
+}
+else {
+  console.log('values are not equal');
+}
+
+// type coercion
+// type conversoin
+// type casting
